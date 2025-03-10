14e64561a9d5069290ceb752098bbe92295c4439
PhoebeGale <phoebegalelim1@gmail.com>
2025-03-10 10:38:49 +0800

    Add files via upload

---
+++ StudentAPI/contract/IBaseRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace contract;
+
+interface IBaseRepository {
+    public function getAll();
+    public function findById($id);
+    public function create($data);
+    public function update($id, $data);
+    public function delete($id);
+}
